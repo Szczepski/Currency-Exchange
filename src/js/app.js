d91536d6685031482878a4cd42879af1d93abd4e
Szczepan Śliwiński <szczepan.sliwinski@gmail.com>
2017-09-12 09:56:58 +0200

    currency table from NBP API, flags included

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,76 @@ $(() => {
             let newTabNr = response[0].no;
             let newTabDate = response[0].effectiveDate;
 
+        // Przypisanie nazwy pobranej tablicy:
+            tabHead.text("Tabela z kursami walut NBP nr " + newTabNr + " z dnia " + newTabDate);
+
+        // Pobranie walut
+            let currencyObjArr = response[0].rates;
+            //console.log(currencyArr);
+
+            currencyObjArr.forEach((currency, i) => {
+                //console.log(currency);
+                let tr = $('<tr>');
+                tabBody.append(tr);
+
+                const currencys = []
+
+                    let currencyInfo = $(currency).map((curency, i) => {
+                        //console.log(currency.currency, currency.code, currency.mid );
+
+
+                        let currencyCode = $('<td>', {class: 'currency-code'});
+                        let currencyCodeText = $('<span>', {class: 'code-text'}).text(currency.code);
+
+                        let currencyName = $('<td>', {class: 'currency-name'}).text(currency.currency);
+
+                        let currencyBid = $('<td>', {class: 'currency-bid'}).text(currency.bid.toFixed(4));
+                        let currencyAsk = $('<td>', {class: 'currency-ask'}).text(currency.ask.toFixed(4));
+
+                        let currencyFlag = $('<span>', { class: 'flag'});
+
+                        let currencyLink = $('<a>', {href: '#'});
+                        let pln = $('<span>', {class: 'pln-text'}).text("/PLN");
+
+                        tr.append(currencyCode).append(currencyName).append(currencyBid).append(currencyAsk);
+                        currencyCode.append(currencyLink.append(currencyCodeText).append(currencyFlag).append(pln));
+
+                    // Prepering to add right background flag class
+                        let bgFlagClass = (currency.code).toLowerCase();
+                        //console.log(flagClass);
+
+                    // Adding to <span class="flag"> right flagClass with background-image
+                        currencyFlag.addClass(bgFlagClass)
+
+                        currencys.push(currency.code)
+
+                    });
+
+
+
+            })
+            //var ask;
+            
+
+
+
+
+            // function count() {
+            //
+            //     const count = $('.onClick'); console.log(count);
+            //
+            //         count.on('click', function() {
+            //             let input = $('input').val();
+            //             let sum = el1Bid * (el2Ask * input);
+            //             console.log(sum);
+            //
+            //         })
+                        //let countMessage = $('<h4>').text("Za " + input + form1 ", kupisz " + sum + form2);
+
+                        //count.append(countMessage);
+            // }
+            // count();
+
 
         })
 
